f75c6a10e65a321dc44094b26bc349ac3f998234
Zoheir El Houari <a12044027@unet.univie.ac.at>
2024-06-02 10:24:32 +0200

    SMIX-2 adding feature and structure oriented augmentations

--- main.py
+++ main.py
@@ -2,19 +2,35 @@ import numpy as np
 
 import time
 import sys
-
+import os 
 from utils import ReadData
 from spectral import Model
 from embedder import Embedder
 from evaluate import clustering
+import csv
+
+def export_results_to_csv(dataset_name, nmi, ari, execution_time, augmentation_method,augmentation_method_exec_time, filename='experiment_results.csv'):
+    """Export experiment results to a CSV file."""
+    header = ['DatasetName', 'NMI', 'ARI', 'ExecutionTime', 'AugmentationMethod', 'AugmentationMethodExecTime']
+    data = [dataset_name, nmi, ari, execution_time, augmentation_method,augmentation_method_exec_time]
+
+    # Check if file exists and write header if it does not
+    write_header = not os.path.isfile(filename)
+
+    with open(filename, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        if write_header:
+            writer.writerow(header)
+        writer.writerow(data)
+    print(f'Results saved to {filename}')
 
 graphName = sys.argv[1]
+augmentation_method = sys.argv[2]
 print(graphName)
 
 dataset = ReadData(graphName)
 dataset.printD()
-dataset.featureAdditionWithNoise(num_features=1500)
-dataset.featureMasking(mask_prob = 0.01)
+
 graphs = dataset.graphs
 node_attr = dataset.node_attr
 num_atts = dataset.atts
@@ -26,11 +42,19 @@ dim = dataset.dim
 dim_cluster = dataset.dim_cluster
 iterations = dataset.iterations
 extra = dataset.extraiter
+
+
+# Apply feature-oriented augmentation
+augmentation_method_exec_time_start = time.time()
+adj, node_attr = dataset.augmentFeatures(augmentation_method)
+augmentation_method_exec_time_end = time.time()
+augmentation_method_exec_time = augmentation_method_exec_time_end - augmentation_method_exec_time_start
+
 test_ids = dataset.test_ids
 val_ids = dataset.val_ids
 train_ids = dataset.train_ids
 
-m = Model(graphs, adj, num_nodes , False, node_attr, num_atts)
+m = Model(graphs, adj, num_nodes , True, node_attr, num_atts)
 
 startIndex = m.startIndex
 sumWeights = m.sumWeights
@@ -81,8 +105,10 @@ nodeEmb = embed.nodeEmbedding
 attEmb = embed.attributeLabelEmbedding
 end = time.time()
 
-
 print("Time : ", end-start)
 print("\nClustering Task:")
 nmi, ari = clustering(nodeEmb, gt, clusters, test_ids)
 print()
+
+# Export results to CSV
+export_results_to_csv(graphName, nmi, ari, end-start, augmentation_method,augmentation_method_exec_time)
